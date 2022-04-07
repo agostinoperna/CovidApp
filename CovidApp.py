1f4ce7e8b0ef89a1b4d377d7b6722a4c19ac12de
agostinoperna <45530501+agostinoperna@users.noreply.github.com>
2022-04-07 08:27:19 +0200

    Update CovidApp.py

--- CovidApp.py
+++ CovidApp.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
-# from plotly.subplots import make_subplots
+from plotly.subplots import make_subplots
 # import plotly.express as px
-import plotly.graph_objects as go
-
+# import plotly.graph_objects as go
+import plotly.figure_factory as ff
 
 
 st.title('Analisi decessi province italiane 2011-2021')
@@ -24,7 +24,7 @@ if uploaded_file:
 			rows = len(province_selezionate)
 			fig=make_subplots(rows = rows, cols = 1, shared_xaxes = True, subplot_titles=province_selezionate)
 			for provincia in province_selezionate:
-				fig.add_trace(go.Scatter( x=df['data'] , y = df[provincia], name=provincia),
+				fig.add_trace(ff.Scatter( x=df['data'] , y = df[provincia], name=provincia),
 							  row=province_selezionate.index(provincia) +1, col=1)
 			fig.update_layout(showlegend=False, plot_bgcolor='rgb(255,255,255)')
 			fig.update_xaxes(showgrid=True, gridwidth=0.05, gridcolor="LightPink",
